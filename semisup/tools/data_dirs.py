67106650f75b644555b78bdb4c29ac729e119fc0
xuji <xj112358@gmail.com>
2018-10-10 19:26:19 +0100

    .

--- semisup/tools/data_dirs.py
+++ semisup/tools/data_dirs.py
@@ -3,17 +3,7 @@ This file contains the directory paths to data sets on your hard drive.
 Change the directories and .gitignore this file.
 """
 
-synth = '/work/haeusser/data/synth/'
-stl10 = '/work/haeusser/data/stl10_binary/'
-svhn =  '/work/haeusser/data/svhn/'
-mnist = '/usr/stud/plapp/data/mnist/'
-fashion_mnist = '/usr/stud/plapp/data/fashion-mnist/'
-imagenet = '/work/haeusser/data/imagenet/raw-data/'
-imagenet_labels = '/usr/wiss/haeusser/libs/tfmodels/inception/inception/data/imagenet_lsvrc_2015_synsets.txt'
-gtsrb = '/work/haeusser/data/gtsrb/'
-usps = '/work/haeusser/data/usps/'
-office = '/work/haeusser/data/office/'
-mnistm = ''
-cifar100 = '/usr/stud/plapp/data/cifar-100-batches-bin'
-cifar10 = '/usr/stud/plapp/data/cifar-10-python'
-norb = '/work/haeusser/data/norb'
+stl10 = '/scratch/local/ssd/xuji/STL/stl10_binary'
+mnist = '/scratch/local/ssd/xuji/mnist' # seems to need unprocessed version
+cifar100 = '/scratch/local/ssd/xuji/CIFAR/cifar-100-python' # seems to need this
+cifar10 = '/scratch/local/ssd/xuji/CIFAR/cifar-10-batches-py'
